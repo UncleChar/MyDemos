0ef78431a54c97015eba85718fc8b33ca672ce69
UncleChar <xhijob@126.com>
2015-12-29 17:16:24 +0800

    提交一个整合过的

---
+++ LaunchViewController.m
@@ -0,0 +1,13 @@
+//
+//  LaunchViewController.m
+//  UncleCharDemos
+//
+//  Created by LingLi on 15/12/28.
+//  Copyright © 2015年 hailong.xie. All rights reserved.
+//
+
+#import "LaunchViewController.h"
+
+@implementation LaunchViewController
+
+@end
